bbef2cb717d139d831245872fa9dde7f7fe9a1f6
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2024-11-02 18:04:36 +0300

    Initial Commit

---
+++ PickerTrain/PickerTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerTrainApp.swift
+//  PickerTrain
+//
+//  Created by Данил Толстиков on 02.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct PickerTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PickerTrainTests/PickerTrainTests.swift
@@ -0,0 +1,17 @@
+//
+//  PickerTrainTests.swift
+//  PickerTrainTests
+//
+//  Created by Данил Толстиков on 02.11.2024.
+//
+
+import Testing
+@testable import PickerTrain
+
+struct PickerTrainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
