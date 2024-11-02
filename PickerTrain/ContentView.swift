34898d8da3db47f162549b3dbdc95277f3ecca82
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2024-11-02 19:24:29 +0300

    Done

--- PickerTrain/ContentView.swift
+++ PickerTrain/ContentView.swift
@@ -1,21 +1,38 @@
-//
-//  ContentView.swift
-//  PickerTrain
-//
-//  Created by Данил Толстиков on 02.11.2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .blue, .yellow]
+    let colorNames: [String] = ["Red", "Blue", "Yellow"]
+    @State var colorIndex = 0
+    @State var isOn = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            VStack{
+                Form {
+                    Section(header: Text("General"), footer: Text("Lets go")) {
+                        Picker(selection: $colorIndex,
+                               label: Label("Color", systemImage: "circle.fill").foregroundStyle(colors[colorIndex]),
+                               content: {
+                            ForEach(0..<colorNames.count, id: \.self) {
+                                Text(colorNames[$0]).tag($0)
+                            }
+                        })
+                        Toggle(isOn: $isOn) {
+                            Label("Enable notifications", systemImage: "bell.fill")
+                        }
+                    }.navigationTitle("Settings")
+                    Section(header: Text("Notifications"), footer: Text("This header is about notifications")) {
+                        Text("Notifications are enabled: \(isOn)")
+                        Button("Toggle notifications") { isOn.toggle() }
+                    }
+                }
+                Text("Settings page").padding()
+            }
+        }.animation(.spring(), value: colorIndex)
+        
+
+            
+        
     }
 }
 
